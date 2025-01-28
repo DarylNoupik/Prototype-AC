8dd28c2c74f565123435b1be359ca7e96c13a80f
DarylNoupik <daryl.kuitche@2023.ucac-icam.com>
2025-01-28 22:55:17 +0100

    ajout de breeze et des migrations

---
+++ database/migrations/2025_01_28_214250_create_cultures_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cultures', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phase'); // Phase de la culture (ex: semis, croissance, récolte)
+            $table->foreignId('site_id')->constrained()->onDelete('cascade'); // Relation avec les sites géographiques
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cultures');
+    }
+};
---
+++ database/migrations/2025_01_28_214301_create_alerts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Relation avec l'utilisateur
+            $table->string('message');
+            $table->string('critical_data'); // Donnée critique associée à l'alerte
+            $table->dateTime('date'); // Date de l'alerte
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alerts');
+    }
+};
