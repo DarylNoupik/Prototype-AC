1c294a3ffff5f6cbcf1f422568fbf58b35ea5c39
LeViolent <bernardmantzo@gmail.com>
2025-05-21 17:12:20 +0100

    adding  migration
    migration table donnees sensorielles

---
+++ database/migrations/2025_05_21_110613_create_sensor_data_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('sensor_data', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('site_id')->constrained()->onDelete('cascade');
+            $table->decimal('temperature', 8, 2); // Increased precision
+            $table->decimal('co2_level', 8, 2); // Renamed
+            $table->decimal('soil_humidity', 8, 2); // Renamed
+            $table->decimal('luminosity', 8, 2); // Renamed
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('sensor_data');
+    }
+};
