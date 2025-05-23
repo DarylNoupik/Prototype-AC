8f3a77835ecc63baca896e93e8bd66584e2f8feb
LeViolent <bernardmantzo@gmail.com>
2025-05-23 14:16:17 +0100

    Update SensorData.php
    changing relations to culture

--- app/Models/SensorData.php
+++ app/Models/SensorData.php
@@ -11,18 +11,18 @@ class SensorData extends Model
 
     protected $table = 'sensor_data';
 
-    protected $fillable = ['temperature', 'luminosity', 'co2_level', 'soil_humidity', 'site_id'];
+    protected $fillable = ['temperature', 'luminosity', 'co2_level', 'soil_humidity', 'cultures_id'];
 
     protected $casts = [
         'temperature' => 'float',
         'luminosity' => 'float',
         'co2_level' => 'float',
         'soil_humidity' => 'float',
-        'site_id' => 'integer',
+        'cultures_id' => 'integer',
     ];
 
-    public function site()
+    public function cultures()
     {
-        return $this->belongsTo(Site::class);
+        return $this->belongsTo(Culture::class);
     }
 }
