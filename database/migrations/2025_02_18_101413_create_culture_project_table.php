b0780f6d06869b9809cceb521c86f4f98b488fc6
LeViolent <bernardmantzo@gmail.com>
2025-05-23 14:19:37 +0100

    Update 2025_02_18_101413_create_culture_project_table.php
    changing migration

--- database/migrations/2025_02_18_101413_create_culture_project_table.php
+++ database/migrations/2025_02_18_101413_create_culture_project_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('culture_project', function (Blueprint $table) {
+        Schema::create('culture_projet', function (Blueprint $table) {
             $table->id();
             $table->foreignId('culture_id')->constrained()->onDelete('cascade');
             $table->foreignId('project_id')->constrained()->onDelete('cascade');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('culture_project');
+        Schema::dropIfExists('culture_projet');
     }
 };
